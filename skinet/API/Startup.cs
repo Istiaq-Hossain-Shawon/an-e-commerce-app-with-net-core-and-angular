c9135caaadb7d4e2cbdf0da5915b6462040c9cab
ShawonBit <hossain.ict@gmail.com>
2021-02-27 22:23:33 +0600

    add connection string change package compatible to dotnet version 5.0.3

--- skinet/API/Startup.cs
+++ skinet/API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,7 @@ namespace API
         {
 
             services.AddControllers();
+            services.AddDbContext<StoreContext>(x=> x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))) ;
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });

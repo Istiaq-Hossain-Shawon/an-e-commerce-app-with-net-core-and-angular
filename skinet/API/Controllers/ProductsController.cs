bbef2f8b0a05a442ec688ee534de69e0167bac4d
ShawonBit <hossain.ict@gmail.com>
2021-03-07 23:11:03 +0600

    use product repository in product controller

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,27 +13,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _storeContext;
-
-        public ProductsController(StoreContext storeContext)
-        {
-            _storeContext = storeContext;
-
+        public readonly IProductRepository _productRepository ;
+        public ProductsController(IProductRepository productRepository)
+        {            
+            _productRepository = productRepository;
         }
-
-
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products= await _storeContext.Products.ToListAsync();
+            var products = await _productRepository.GetProductsAsync();
             return Ok(products);
 
         }
-
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> getProduct(int id)
         {
-            return await _storeContext.Products.FindAsync(id);
+            return await _productRepository.GetProductByIdAsync(id);
         }
     }
 }
